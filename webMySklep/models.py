d8e1d5b955620733e9bc455453b68ebfc2952d8b
Marcin Lesiak <marcinui07@gmail.com>
2025-02-26 15:28:34 +0100

    added model for storing shop items

--- webMySklep/models.py
+++ webMySklep/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField()
+    
+    def __str__(self):
+        return self.name
