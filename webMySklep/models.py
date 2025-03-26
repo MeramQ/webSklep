cd2c5153b73229e2cad61541a854010a4cbfed2d
Marcin Lesiak <marcinui07@gmail.com>
2025-03-26 12:52:38 +0100

    updated model to fix uploading images

--- webMySklep/models.py
+++ webMySklep/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     price = models.FloatField()
     stock = models.IntegerField(default=0)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     
     def __str__(self):
         return self.name
